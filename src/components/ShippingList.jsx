6b3fbb4a55a80c52e6db2f6059dda74ce39c7dc6
IamTeju <ntejaswini308@gmail.com>
2022-09-24 13:42:00 +0530

    initial commit

---
+++ src/components/ShippingList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useState } from 'react'
+import ShippingForm from './ShippingForm';
+
+function ShippingList() {
+    const [items, setItems] = useState([]);
+    const addToList = (item) => {
+        console.log(item);
+        const itemsCopy = [...items];
+        itemsCopy.push(item)
+        setItems(itemsCopy);
+
+    };
+    return (
+        <div>
+            <ShippingForm addToListCallback={(value) => addToList(value)} />
+            <table>
+                <thead>
+                    <th>Title</th>
+                    <th>Color</th>
+                    <th>Weight</th>
+                    <th> Place</th>
+                </thead>
+                <tbody>
+                    {
+                        items.map(items => {
+                            return (
+                                <tr key={items.title}>
+                                    <td> {items.title}</td>
+                                    <td> {items.color}</td>
+                                    <td> {items.weight}</td>
+                                    <td> {items.place}</td>
+                                </tr>
+
+                            )
+                        }
+                 
+                  ) }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ShippingList
